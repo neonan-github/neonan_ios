4f6dea3a954e48cbe8fe7e117acdb60fe1ca9677
daCapricorn <capricorn0113@gmail.com>
2012-10-18 19:20:25 +0800

    shrinkable-textview

---
+++ Classes/helpers/UIHelper.m
@@ -0,0 +1,29 @@
+//
+//  UIHelper.m
+//  Neonan
+//
+//  Created by capricorn on 12-10-18.
+//  Copyright (c) 2012年 neonan. All rights reserved.
+//
+
+#import "UIHelper.h"
+
+@implementation UIHelper
++ (void)fitScrollView:(UIScrollView *)scrollView withMaxHeight:(float)maxHeight {
+    CGRect frame = scrollView.frame;
+    frame.size.height = MIN(maxHeight, scrollView.contentSize.height);
+    scrollView.frame = frame;
+}
+
++ (void)view:(UIView *)view alignBottomTo:(float)bottom {
+    CGRect frame = view.frame;
+    frame.origin.y = bottom - frame.size.height;
+    view.frame = frame;
+}
+
++ (void)makeTextViewAlignCenter:(UITextView *)tv {
+    CGFloat topCorrect = ([tv bounds].size.height - [tv contentSize].height * [tv zoomScale])/2.0;
+    topCorrect = ( topCorrect < 0.0 ? 0.0 : topCorrect );
+    tv.contentOffset = (CGPoint){.x = 0, .y = -topCorrect};
+}
+@end
---
+++ Classes/helpers/UILabel+dynamicSizeMe.h
@@ -0,0 +1,16 @@
+//
+//  UILabel+dynamicSizeMe.h
+//  Neonan
+//
+//  Created by capricorn on 12-10-18.
+//  Copyright (c) 2012年 neonan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (dynamicSizeMe)
+
+-(float)resizeToFit;
+-(float)expectedHeight;
+
+@end
