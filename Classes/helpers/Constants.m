da19d3268910b2343c3ff1fad4312e08b49b10be
daCapricorn <capricorn0113@gmail.com>
2013-03-11 19:20:43 +0800

    vote url

--- Classes/helpers/Constants.m
+++ Classes/helpers/Constants.m
@@ -9,10 +9,10 @@ NSString *const kPathPublishComment = @"neonan/comments_create";
 NSString *const kPathPeopleList = @"neonan/subject/people_list";
 NSString *const kPathPeopleInfo = @"neonan/subject/people";
 NSString *const kPathNearPeople = @"neonan/subject/near_people";
-NSString *const kPathPeopleVote = @"haha/vote";
 NSString *const kPathCreateOrder = @"neonan/create_order";
 NSString *const kPathAddPoint = @"neonan/add_point";
 
 NSString *const kPathNeoNanSignUp = @"api/register";
 NSString *const kPathNeoNanLogin = @"api/login";
-NSString *const kPath3rdLogin = @"haha/api/login3rd";
+NSString *const kPath3rdLogin = @"api/login3rd";
+NSString *const kPathPeopleVote = @"api/vote";
