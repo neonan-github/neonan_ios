ad66f9048e9a7ceb87997d3c00b20a78448b311f
daCapricorn <capricorn0113@gmail.com>
2013-05-24 12:13:36 +0800

    basic layout of HomeViewController

--- Classes/controllers/HomeViewController.m
+++ Classes/controllers/HomeViewController.m
@@ -53,6 +53,12 @@ AQGridViewDelegate, AQGridViewDataSource>
     self.swipeView = nil;
 }
 
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    [self.swipeView reloadData];
+}
+
 #pragma mark - SwipeViewDataSource methods
 
 - (NSInteger)numberOfItemsInSwipeView:(SwipeView *)swipeView {
@@ -62,11 +68,23 @@ AQGridViewDelegate, AQGridViewDataSource>
 - (UIView *)swipeView:(SwipeView *)swipeView viewForItemAtIndex:(NSInteger)index reusingView:(UIView *)view {
     AQGridView *gridView = (AQGridView *)view;
     
-    if (!view) {
-        gridView = [[AQGridView alloc] initWithFrame:self.view.bounds];
+    if (!view) {   
+        gridView = [[AQGridView alloc] initWithFrame:CGRectMake(0, 0, CompatibleScreenWidth, CompatibleContainerHeight)];
+        gridView.contentSizeGrowsToFillBounds = NO;
+        gridView.leftContentInset = 5;
+//        gridView.rightContentInset = 10;
+//        gridView.layoutDirection = AQGridViewLayoutDirectionHorizontal;
+        gridView.backgroundColor = [UIColor redColor];
         gridView.dataSource = self;
         gridView.delegate = self;
+        
+        UIImageView *headerImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 300, 167)];
+        headerImageView.backgroundColor = [UIColor greenColor];
+        gridView.gridHeaderView = headerImageView;
     }
+    
+    gridView.tag = index;
+    [gridView reloadData];
     
     return gridView;
 }
@@ -82,11 +100,15 @@ AQGridViewDelegate, AQGridViewDataSource>
     
     HomeGridViewCell * cell = (HomeGridViewCell *)[gridView dequeueReusableCellWithIdentifier:CellIdentifier];
     if (!cell) {
-        cell = [[HomeGridViewCell alloc] initWithFrame: CGRectMake(0.0, 0.0, 120.0, 150.0) reuseIdentifier:CellIdentifier];
-        cell.backgroundColor = [UIColor redColor];
+        cell = [[HomeGridViewCell alloc] initWithFrame:CGRectMake(0.0, 0.0, 145.0, 116.0) reuseIdentifier:CellIdentifier];
+        cell.contentView.backgroundColor = [UIColor blueColor];
     }
     
     return cell;
+}
+
+- (CGSize)portraitGridCellSizeForGridView:(AQGridView *)gridView {
+    return CGSizeMake(155.0, 126.0);
 }
 
 @end
